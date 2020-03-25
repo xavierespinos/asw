23d2eefb47e1ebe54a3906064b6e6c20c3d455dd
marcel costa <marcel.dfga@gmail.com>
2020-03-25 12:44:53 +0000

    Ordena per Points

--- app/models/contribucion.rb
+++ app/models/contribucion.rb
@@ -1,2 +1,4 @@
 class Contribucion < ApplicationRecord
+	#belongs_to :user
+	default_scope -> { order(points: :desc) }
 end
--- test/models/contribucion_test.rb
+++ test/models/contribucion_test.rb
@@ -4,4 +4,9 @@ class ContribucionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "order should be most recent first" do
+    assert_equal contribucions(:most_recent), Contribucion.first
+  end
+  
 end
