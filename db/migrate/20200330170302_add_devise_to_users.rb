b2ee8225e5e6fe8715e2559f3b5cc30855771921
Marcel Costa <marcel.dfga@gmail.com>
2020-03-31 14:10:05 +0000

    m

--- db/migrate/20200330170302_add_devise_to_users.rb
+++ db/migrate/20200330170302_add_devise_to_users.rb
@@ -11,6 +11,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+
       ## Rememberable
       t.datetime :remember_created_at
 
