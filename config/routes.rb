dbedf749a46f1f217c8e604f0325b89cc85ac344
Ubuntu <ubuntu@ip-172-31-32-167.ec2.internal>
2020-04-12 00:02:45 +0000

    Edit profile fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get '/contribucions/newPage', to: 'contribucions#newPage', action: :newPage , controller: 'contribucions'
   get '/users/:id' =>'users#show'
-  get '/users/:id/edit' => 'users#edit'
+  get '/users/:id/edit', :to => 'users#edit', :as => :user
 
   resources :contribucions do
     member do
