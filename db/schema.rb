7fd60a0ae39f79ec1a1c5ebf55bb814975351abe
Sergio.Pascual <sergiopg10@hotmail.es>
2020-04-17 17:53:38 +0000

    Corregit descripcio usuari. No funciona el save de l'usuari

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_171223) do
+ActiveRecord::Schema.define(version: 2020_04_17_173055) do
 
   create_table "comentaris", force: :cascade do |t|
     t.string "text", null: false
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2020_04_12_171223) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comentaris_on_commentable_type_and_commentable_id"
   end
 
   create_table "contribucions", force: :cascade do |t|
@@ -44,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_171223) do
     t.datetime "remember_created_at"
     t.string "provider"
     t.string "uid"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
