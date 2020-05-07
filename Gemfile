33a74550bb50be4ff3f950023b86c5b7a9cfe47e
Ubuntu <ubuntu@ip-172-31-45-152.ec2.internal>
2020-05-07 23:00:03 +0000

    Update Gemfile for Heroku

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ end
 
 group :production do
   gem 'pg', '1.1.4'
+  gem "rspec-rails"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/integration/contribucions_spec.rb
+++ spec/integration/contribucions_spec.rb
@@ -1,31 +1,6 @@
 # spec/integration/contribucions_spec.rb
 require 'swagger_helper'
 
-describe 'Hackersnews API' do
 
-  path '/apps/controllers/api/contribucions/{id}' do
 
-    get 'Retorna totes les contribucions' do
-      tags 'Contribucions'
-      produces 'application/json', 'application/xml'
-      parameter name: :id, :in => :path, :type => :string
-
-      response '200', 'name found' do
-        schema type: :object,
-          properties: {
-            id: { type: :integer, },
-            name: { type: :string },
-          },
-          required: [ 'id', 'name', 'status' ]
-
-        let(:id) { Contribucions.create(titol: 'foo')}
-        run_test!
-      end
-
-      response '404', 'pet not found' do
-        let(:id) { 'invalid' }
-        run_test!
-      end
-    end
-  end
-end
+ 
