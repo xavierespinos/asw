fcd0cecb98b0a72900690ead5589791515b6c9e6
Ubuntu <ubuntu@ip-172-31-45-152.ec2.internal>
2020-05-08 00:40:31 +0000

    Update Gemfile for Heroku

--- db/migrate/20200507192949_create_comentaris_voteds.rb
+++ db/migrate/20200507192949_create_comentaris_voteds.rb
@@ -1,7 +1,7 @@
 class CreateComentarisVoteds < ActiveRecord::Migration[6.0]
   def change
     create_table :comentaris_voteds do |t|
-      t.integer :user
+      t.integer :uid
       t.integer :comentari
 
       t.timestamps
